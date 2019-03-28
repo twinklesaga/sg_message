87f2209d530f999118c82e2557657fd73bb40063
김형진/SK 테크엑스(twinkle@sk.com) <twinkle@sk.com>
2019-03-28 22:45:39 +0900

    resouce 수정

--- sg_resource.go
+++ sg_resource.go
@@ -3,4 +3,7 @@ package sg_message
 type Resource struct {
 	Path 		string			`json:"path"`
 	Bitrate 	int 			`json:"bitrate"`
+	Target 		string 			`json:"target"`
+	UploadType	string			`json:"uploadType"`
+	Type 		string 			`json:"type"`
 }
