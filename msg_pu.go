885994109cd8352c3934e45b613565025ebe2b63
김형진/SK 테크엑스(twinkle@sk.com) <twinkle@sk.com>
2019-03-28 22:43:53 +0900

    pu message  수정

--- msg_pu.go
+++ msg_pu.go
@@ -4,15 +4,16 @@ type MsgPubPayload struct {
 	RhId 			int			`json:"rhId"`
 	RhName 			string		`json:"rhName"`
 	MimeType		string 		`json:"mimeType"`
+	ImageType		string 		`json:"imageType"`
 	Workspace 		string		`json:"workspace"`
 	ManifestFile 	string		`json:"manifestFile"`
 	Resource 		[]Resource	`json:"resources"`
 	AlbumId 		int			`json:"albumId"`
 	TrackId 		int 		`json:"trackId"`
+	ArtistId		int			`json:"artistId"`
 	ResultDir 		string		`json:"resultDir"`
 	Key				string 		`json:"key"`
-	Eq 				string 		`json:"eq"`
-	TrackLoudness	string		`json:"trackLoudness"`
+	TrackLoudness	string 		`json:"trackLoudness"`
 	Duration		int			`json:"duration"`
 	SourceType		string 		`json:"sourceType"`
 	OriginFile 		string 		`json:"originFile"`
