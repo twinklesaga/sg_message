49f8dfdcc18210187a847fbdec37df56279cfe9c
김형진/SK 테크엑스(twinkle@sk.com) <twinkle@sk.com>
2019-04-04 20:46:57 +0900

    msg px

---
+++ msg_px.go
@@ -0,0 +1,22 @@
+package sg_message
+
+
+type MsgPXPayload struct {
+	RhId 			int				`json:"rhId"`
+	RhName 			string			`json:"rhName"`
+	OrgWorkspace 	string 			`json:"orgWorkspace"`
+	Workspace 		string 			`json:"workspace"`
+	ManifestFile 	string 			`json:"manifestFile"`
+	Resources 		[]PXResource  	`json:"resources"`
+	ResultDir 		string 			`json:""`
+}
+
+type PXResource struct {
+	Path 			string 			`json:"path"`
+	LastModified 	int64 			`json:"lastModified"`
+}
+
+type MsgPX struct {
+	Header 			MsgHeader 		`json:"header"`
+	Payload 		MsgPXPayload 	`json:"payload"`
+}
