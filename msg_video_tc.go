9e4b8d3220e846d21666140a481f8dd0ca1ef00f
김형진/SK 테크엑스(twinkle@sk.com) <twinkle@sk.com>
2019-06-30 16:07:23 +0900

    video tc message 추가

---
+++ msg_video_tc.go
@@ -0,0 +1,19 @@
+package sg_message
+
+type MsgVideoTcPayload struct {
+	RhId 			int        `json:"rhId"`
+	RhName 			string   `json:"rhName"`
+	Workspace 		string    `json:"workspace"`
+	ManifestFile 	string     `json:"manifestFile"`
+	Resource 		[]Resource `json:"resources"`
+	AlbumId 		int         `json:"albumId"`
+	TrackId 		int         `json:"trackId"`
+	VideoId			int 		`json:"videoId"`
+	VideoType		string 		`json:"videoType"`
+	ResultDir 		string    `json:"resultDir"`
+}
+
+type MsgVideoTc struct {
+	Header 		MsgHeader 			`json:"header"`
+	Payload 	MsgVideoTcPayload	`json:"payload"`
+}
