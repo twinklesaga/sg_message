e343ec0f6bfee9ac11691af1eea533f3b8e1ac0b
김형진/SK 테크엑스(twinkle@sk.com) <twinkle@sk.com>
2019-12-31 15:35:04 +0900

    video message process type 추가

--- msg_video_tc.go
+++ msg_video_tc.go
@@ -3,6 +3,7 @@ package sg_message
 type MsgVideoTcPayload struct {
 	RhId 			int        `json:"rhId"`
 	RhName 			string   `json:"rhName"`
+	Process 		string 		`json:"process"`
 	Workspace 		string    `json:"workspace"`
 	ManifestFile 	string     `json:"manifestFile"`
 	Resource 		[]Resource `json:"resources"`
